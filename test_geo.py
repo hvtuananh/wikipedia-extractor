7daf5dcc67ffd9d04438253203f2647557b623c7
tuananh <hvtuananh@gmail.com>
2014-09-18 16:41:17 -0400

    dealing with more malformed cases. there are 200+ articles that i cant extract coordinate, but thats good enough

--- test_geo.py
+++ test_geo.py
@@ -4,8 +4,8 @@ import datetime
 
 p = re.compile(r'[^a-z0-9#]+', re.I)
 p_id = re.compile(r'id=(\d+)')
-p_geo = re.compile(r'{{(coord|coorde|coordinate)s?\s?\|(.+?)\|([A-Z])\s*?\|(.+?)\|([A-Z])\s*?(\|?}}|\|(.+?)}})', re.I)
-p_geo_a = re.compile(r'{{(coord|coorde|coordinate)s?\s?\|([^\|]+?)\|+?([^\|]+?)(\|?}}|\|+?(.+?)}})', re.I)
+p_geo = re.compile(r'{{(coord|coorde|coordinate|coordinite)s?\s?\|((name|text|format)=.+?\|)?(.+?)\|\s*?([A-Z])\s*?\|(.+?)\|\s*?([A-Z])\s*?(\|?}}|\|(.+?)}})', re.I)
+p_geo_a = re.compile(r'{{(coord|coorde|coordinate|coordinite)s?\s?\|((name|text|format)=.+?\|)?([^\|]+?)\|+?([^\|]+?)(\|?}}|\|+?(.+?)}})', re.I)
 
 def convert_arc(text, direction = None):
     result = None
@@ -28,25 +28,29 @@ def convert_arc(text, direction = None):
 
 def extract_geo(text):
     # test malformed cases
-    if 'coord missing' in text.lower() or 'coords missing' in text.lower() or 'coords||' in text.lower() or 'coord||' in text.lower() or 'coord unknown' in text.lower() or 'coord|{{#expr' in text.lower() or '{{coord|}}' in text.lower() or 'coord ' in text.lower() or '{{coord|display' in text.lower() or '{{coorddisplay' in text.lower() or '{{coord|21.0367}}' in text.lower() or 'coord41' in text.lower() or '{{coordinates missing' in text.lower() or '{{coordinates|}}' in text.lower() or '{{coord}}' in text.lower() or '{{coordinate}}' in text.lower() or '{{coordinate/dms|' in text.lower():
+    if 'coord missing' in text.lower() or 'coords missing' in text.lower() or 'coords||' in text.lower() or 'coord||' in text.lower() or 'coord unknown' in text.lower() or 'coord|{{#expr' in text.lower() or '{{coord|}}' in text.lower() or 'coord ' in text.lower() or '{{coord|display' in text.lower() or '{{coorddisplay' in text.lower() or '{{coord|21.0367}}' in text.lower() or 'coord41' in text.lower() or '{{coordinates missing' in text.lower() or '{{coordinates|}}' in text.lower() or '{{coord}}' in text.lower() or '{{coordinate}}' in text.lower() or '{{coordinate/dms|' in text.lower() or '{{coordinites!&gt;' in text.lower() or 'coord: +16' in text.lower() or '{{coordinates-inline' in text.lower():
         return None
-    print text.strip()
+    # print text.strip()
     geo = []
     m1 = p_geo.search(text)
     if not m1:
         m2 = p_geo_a.search(text)
     if m1:
-        geo.append(convert_arc(m1.group(2), m1.group(3).strip()))
-        geo.append(convert_arc(m1.group(4), m1.group(5).strip()))
+        geo.append(convert_arc(m1.group(4).strip(), m1.group(5).strip()))
+        geo.append(convert_arc(m1.group(6).strip(), m1.group(7).strip()))
     elif m2:
-        geo.append(convert_arc(m2.group(2)))
-        geo.append(convert_arc(m2.group(3)))
+        geo.append(convert_arc(m2.group(4).strip()))
+        geo.append(convert_arc(m2.group(5).strip()))
     else:
+        print text.strip()
         print "Cannot get correct geo location!"
-        sys.exit(1)
-        
-    print geo
-    print
+    
+    if len(geo) == 2:
+        if (geo[0] is not None and geo[1] is None) or (geo[1] is not None and geo[0] is None):
+            print text.strip()
+            print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    # print geo
+    # print
     return geo
 
 def process_data(input):
